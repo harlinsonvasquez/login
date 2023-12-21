567db390a3ab535df9451bd7be93a29eee3ad8e1
harlinsonvasquez <harvack25@gmail.com>
2023-12-20 21:35:19 -0500

    Add files via upload

--- login/login.js
+++ login/login.js
@@ -1,31 +1,35 @@
 function iniciarSesion(event) {
   event.preventDefault(); // Evitar el envío por defecto del formulario
 
-  var username = document.getElementById("exampleInputEmail1").value;
+  var email = document.getElementById("exampleInputEmail1").value;
   var password = document.getElementById("exampleInputPassword1").value;
 
   const usuarios = [
     {
+      nombre: "harry",
       email: "harry@gmail.com",
       password: "123",
     },
     {
+      nombre: "riwi",
       email: "riwi@gmail.com",
       password: "1234",
     },
     {
+      nombre: "maxi",
       email: "maxi@gmail.com",
       password: "12345",
     },
   ];
 
   const usuarioEncontrado = usuarios.find(
-    (usuario) => usuario.email === username && usuario.password === password
+    (usuario) => usuario.email === email && usuario.password === password
   );
 
   if (usuarioEncontrado) {
+    sessionStorage.setItem('nombre', usuarioEncontrado.nombre);
     console.log("Usuario encontrado");
-    window.location.href = "index.html";
+    window.location.href = "home.html";
   } else {
     console.log("Credenciales inválidas");
   }
