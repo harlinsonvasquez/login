7d71b01c171a8a97f319aa2e99ebe723bf102842
harlinsonvasquez <harvack25@gmail.com>
2023-12-21 21:06:04 -0500

    Add files via upload

--- login/login.js
+++ login/login.js
@@ -1,3 +1,4 @@
+
 function iniciarSesion(event) {
   event.preventDefault(); // Evitar el envío por defecto del formulario
 
@@ -9,16 +10,53 @@ function iniciarSesion(event) {
       nombre: "harry",
       email: "harry@gmail.com",
       password: "123",
+      imagenes:[
+        './images/img-1.jpg',
+                './images/img-2.jpeg',
+                './images/img-3.jpg',
+                './images/img-4.jpg',
+                './images/img-5.webp',
+                './images/img-6.jpg',
+                './images/img-7.jpg',
+                './images/img-8.jpg',
+                './images/img-9.webp',
+                './images/img-10.jpeg',
+
+      ]
     },
     {
       nombre: "riwi",
       email: "riwi@gmail.com",
       password: "1234",
+      imagenes : [
+        './images/img-11.jpg',
+        './images/img-12.jpg',
+        './images/img-13.jpeg',
+        './images/img-14.webp',
+        './images/img-15.jpg',
+        './images/img-16.jpg',
+        './images/img-17.webp',
+        './images/img-18.jpg',
+        './images/img-19.jpg',
+        './images/img-20.jpg',
+    ]
     },
     {
       nombre: "maxi",
       email: "maxi@gmail.com",
       password: "12345",
+      imagenes : [
+        './images/img-11.jpg',
+        './images/img-12.jpg',
+        './images/img-13.jpeg',
+        './images/img-14.webp',
+        './images/img-15.jpg',
+        './images/img-16.jpg',
+        './images/img-17.webp',
+        './images/img-18.jpg',
+        './images/img-19.jpg',
+        './images/img-20.jpg',
+    ]
     },
   ];
 
